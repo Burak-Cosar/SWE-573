3b5fd003e0c40be90f59710a22d77b244be7531a
Burak Cosar <csr.burak@icloud.com>
2024-04-16 21:33:54 +0300

    App name updated to SocialHub

---
+++ SocialHub/SocialHub/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.apps import apps
+
+def get_community_membership_model():
+#Retrieve the CommunityMembership model dynamically to avoid circular imports.
+    return apps.get_model('application', 'CommunityMembership')
+
+# Create your models here.
+class SocialHubUser(AbstractUser):
+    def is_member(self, community_id):
+        CommunityMembership = get_community_membership_model()
+        return CommunityMembership.objects.filter(community_id=community_id, user=self).exists()
